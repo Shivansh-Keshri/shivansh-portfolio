0381e32d8c9bad30f0952617fbf183e7c0bbffb6
Shivansh-Keshri <shivansh.aadi0111@gmail.com>
2024-07-16 18:37:38 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["*"],
+//   theme: {
+//     extend: {
+      
+//     },
+//   },
+//   plugins: [],
+// }
+
+
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      // fontFamily: {
+      //   customFont: ['"Cabinet Grotesk"'],
+      // },
+    },
+  },
+  // Other Tailwind configuration settings
+};
